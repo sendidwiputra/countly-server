49c2b101321b345d687bf81ab546cd1470f86891
Sendi Dwi Putra <sendy_dwiputra@yahoo.com>
2017-07-06 16:46:41 +0700

    update test script

--- plugins/times-of-day/tests.js
+++ plugins/times-of-day/tests.js
@@ -3,7 +3,6 @@ var should = require('should');
 var testUtils = require("../../test/testUtils");
 request = request(testUtils.url);
 
-var API_KEY_ADMIN = "";
 var APP_ID = "";
 var APP_KEY = "";
 var DEVICE_ID = "123456789";
@@ -12,13 +11,13 @@ describe('Testing Times of Day plugin', function() {
     
     describe('Creating event', function(){
 		it('should success', function(done){
-            API_KEY_ADMIN = testUtils.get("API_KEY_ADMIN");
 			APP_ID = testUtils.get("APP_ID");
 			APP_KEY = testUtils.get("APP_KEY");
-            
-            var events = [{"key":event,"count":1,"sum":1,"segmentation":{"app_version":"1.23","platform":"iOS"}}];
-			request
-			.get('/i?app_key=' + APP_KEY + '&device_id=' + DEVICE_ID + "&begin_session=1&events=" + JSON.stringify(events))
+            var hour=new Date().getHours();
+            var dow=new Date().getDay();
+            var events = [{"key":event,hour:hour,dow:dow,"count":1,"sum":1,"segmentation":{"app_version":"1.23","platform":"iOS"}}];
+            request
+			.get('/i?app_key=' + APP_KEY + '&device_id=' + DEVICE_ID + "&begin_session=1&events=" + encodeURIComponent(JSON.stringify(events)))
 			.expect(200)
 			.end(function(err, res){
 				if (err) return done(err);
@@ -31,9 +30,10 @@ describe('Testing Times of Day plugin', function() {
 
     describe('Verify times of day', function(){
         it('should have data', function(done) {
+            API_KEY = testUtils.get("API_KEY");
             APP_ID = testUtils.get("APP_ID") || APP_ID ;
             var data;
-            request.get('/o?method=timesofday&event='+encodeURIComponent(event)+'&api_key='+API_KEY_ADMIN+'&app_id='+APP_ID)
+            request.get('/o?method=timesofday&event='+encodeURIComponent(event)+'&api_key='+API_KEY+'&app_id='+APP
             .end(function(err, res) {
                 res.statusCode.should.equal(200);
                 data = JSON.parse(res.text);
